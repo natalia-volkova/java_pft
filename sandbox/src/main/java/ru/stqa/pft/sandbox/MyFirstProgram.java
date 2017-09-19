0cc3ddcae1f4d1f6a60875c184ba990cd4fb9e6f
Natalia Volkova <orishnat@mail.ru>
2017-09-19 21:09:35 +0300

    Реализовано вычисление расстояния с помощью метода в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,21 +2,26 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-	public static void main (String[] args){
-	System.out.println("Hello, world!");
-	double x1=  3;
-	double y1 = 8;
-	double x2 = 7;
-	double y2 = 12;
+  public static void main(String[] args) {
 
-	Point p1 = new Point(x1, y1);
-	Point p2 = new Point(x2, y2);
+    double x1 = 3;
+    double y1 = 8;
+    double x2 = 4;
+    double y2 = 9;
 
-		System.out.println("The distance between point1 (" + p1.x + "," + p1.y + ") and point2 (" + + p2.x + "," + p2.y + ") is: " + distance(p1, p2));
+    Point p1 = new Point(x1, y1);
+    Point p2 = new Point(x2, y2);
 
-	}
+    System.out.println("The distance between point1 (" + p1.x + "," + p1.y + ") and point2 (" + +p2.x + "," + p2.y + ") is: " + distance(p1, p2)+" :calculated via function.");
+    System.out.println("The distance between point1 (" + p1.x + "," + p1.y + ") and point2 (" + +p2.x + "," + p2.y + ") is: " + p1.distance(p2)+ " :calculated via class method.");
 
-	public static double distance(Point p1, Point p2){
-		 return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
-	}
+  }
+
+  public static void hello(String name){
+    System.out.println("Hello," + name+"!");
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+  }
 }
