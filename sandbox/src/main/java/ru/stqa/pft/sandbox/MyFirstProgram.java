240ffda6153bd189b7f927ac017eab2797e0ea09
Natalia Volkova <orishnat@mail.ru>
2017-09-19 20:57:11 +0300

    Добавлена функция вычисления расстояния между точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,8 +2,21 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
 
-	public static void main (String[] args){	
+	public static void main (String[] args){
 	System.out.println("Hello, world!");
+	double x1=  3;
+	double y1 = 8;
+	double x2 = 7;
+	double y2 = 12;
 
+	Point p1 = new Point(x1, y1);
+	Point p2 = new Point(x2, y2);
+
+		System.out.println("The distance between point1 (" + p1.x + "," + p1.y + ") and point2 (" + + p2.x + "," + p2.y + ") is: " + distance(p1, p2));
+
+	}
+
+	public static double distance(Point p1, Point p2){
+		 return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
 	}
 }
